7bae1d556af992d802130685e8813a6426e9acb7
Apurva Reddy <43247045+apurvareddyk@users.noreply.github.com>
2024-10-03 22:40:37 -0700

    Update TestClass.java

--- src/test/java/src/test/java/com/yourpackage/TestClass.java
+++ src/test/java/src/test/java/com/yourpackage/TestClass.java
@@ -1,1 +1,13 @@
+package com.yourpackage;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestClass {
+
+    @Test
+    void sampleTest() {
+        assertEquals(1, 1); // A simple assertion test
+    }
+}
 
